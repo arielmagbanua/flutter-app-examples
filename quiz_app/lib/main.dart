a21174b26e9c9f354e3a0a95cb8c1cdca16c1813
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-06-04 22:28:55 +0800

    Added styling to the questions widget.

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizapp/question.dart';
 
 void main() => runApp(MyApp());
 
@@ -33,7 +34,7 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Text(questions[_questionIndex]),
+            Question(questions[_questionIndex]),
             RaisedButton(
               child: Text('Answer 1'),
               onPressed: _answerQuestion,
