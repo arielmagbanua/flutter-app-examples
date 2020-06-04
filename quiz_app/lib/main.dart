ce1aacc8ac6ee64f957f324da6009e59a36e6c24
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-06-04 20:09:03 +0800

    Added basic layour.

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'));
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
+        ),
+        body: Text("Body Hello World hehe...."),
+      ),
+    );
   }
 }
