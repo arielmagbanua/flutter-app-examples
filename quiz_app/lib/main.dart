6974264bb643fc04d08b0bafb60e03547b99c58d
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-06-07 21:54:44 +0800

    Calculating score and displaying it in result widget.

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -12,38 +12,39 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var _questionIndex = 0;
   final _questions = const [
     {
       'questionText': "What's your favorite color?",
       'answers': [
-        'Black',
-        'Red',
-        'Green',
-        'White',
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
       ],
     },
     {
       'questionText': "What's your favorite animal?",
       'answers': [
-        'Rabbit',
-        'Snake',
-        'Elep/hant',
-        'Lion',
+        {'text': 'Rabbit', 'score': 1},
+        {'text': 'Snake', 'score': 10},
+        {'text': 'Elephant', 'score': 3},
+        {'text': 'Lion', 'score': 5},
       ],
     },
     {
       'questionText': "What's your favorite instructor?",
       'answers': [
-        'Max',
-        'Ariel',
-        'Brent',
-        'John',
+        {'text': 'Max', 'score': 10},
+        {'text': 'Ariel', 'score': 1},
+        {'text': 'Brent', 'score': 3},
+        {'text': 'John', 'score': 5},
       ],
     },
   ];
+  var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -51,6 +52,8 @@ class _MyAppState extends State<MyApp> {
     if (_questionIndex < _questions.length) {
       print('We have more questions!');
     }
+
+    _totalScore += score;
 
     print(_questionIndex);
   }
@@ -68,7 +71,7 @@ class _MyAppState extends State<MyApp> {
                 answerQuestion: _answerQuestion,
                 questionIndex: _questionIndex,
               )
-            : Result(),
+            : Result(_totalScore),
       ),
     );
   }
--- quiz_app/lib/result.dart
+++ quiz_app/lib/result.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final resultScore;
+
+  Result(this.resultScore);
+
+  String get resultPhrase {
+    String resultText;
+
+    if (resultScore <= 8) {
+      resultText = 'You are awesome and innocent!';
+    } else if (resultScore <= 12) {
+      resultText = 'Pretty Likeable!';
+    } else if (resultScore <= 16) {
+      resultText = 'You are ... strange?';
+    } else {
+      resultText = 'You are so bad!';
+    }
+
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You did it!'),
+      child: Text(
+        resultPhrase,
+        style: TextStyle(
+          fontSize: 36,
+          fontWeight: FontWeight.bold,
+        ),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
