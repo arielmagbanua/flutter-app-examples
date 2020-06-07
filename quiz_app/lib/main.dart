1b38a15c47ab3b5edbdeb3fb6d67d499b7c58e18
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-06-07 22:17:32 +0800

    Added reseting of quiz.

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -41,8 +41,16 @@ class _MyAppState extends State<MyApp> {
       ],
     },
   ];
+
   var _questionIndex = 0;
   var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
 
   void _answerQuestion(int score) {
     setState(() {
@@ -71,7 +79,7 @@ class _MyAppState extends State<MyApp> {
                 answerQuestion: _answerQuestion,
                 questionIndex: _questionIndex,
               )
-            : Result(_totalScore),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
--- quiz_app/lib/result.dart
+++ quiz_app/lib/result.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final resultScore;
+  final Function resetQuiz;
 
-  Result(this.resultScore);
+  Result(this.resultScore, this.resetQuiz);
 
   String get resultPhrase {
     String resultText;
@@ -24,13 +25,22 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: TextStyle(
-          fontSize: 36,
-          fontWeight: FontWeight.bold,
-        ),
-        textAlign: TextAlign.center,
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            child: Text('Restart Quiz'),
+            textColor: Colors.blue,
+            onPressed: resetQuiz,
+          )
+        ],
       ),
     );
   }
