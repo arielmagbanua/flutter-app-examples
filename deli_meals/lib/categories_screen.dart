9ddd5bcd662fb57f6193d7fcf7b97274c97d7754
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-06-28 14:27:30 +0800

    Passing information to category meals screen.

--- deli_meals/lib/categories_screen.dart
+++ deli_meals/lib/categories_screen.dart
@@ -13,7 +13,7 @@ class CategoriesScreen extends StatelessWidget {
       body: GridView(
         padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES.map((catData) {
-          return CategoryItem(catData.title, catData.color);
+          return CategoryItem(catData.id, catData.title, catData.color);
         }).toList(),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
--- deli_meals/lib/category_meals_screen.dart
+++ deli_meals/lib/category_meals_screen.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
+  final String categoryId;
+  final String categoryTitle;
+
+  CategoryMealsScreen(this.categoryId, this.categoryTitle);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('The Recipes'),
+        title: Text(categoryTitle),
       ),
       body: Center(
         child: Text('The recipes for the category!'),
