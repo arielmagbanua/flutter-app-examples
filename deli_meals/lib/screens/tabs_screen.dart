363a8bd37e060eca6eca709b64d454299ec2dce2
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-06-29 22:46:15 +0800

    Improved the app bar title as it switches the tab.

--- deli_meals/lib/screens/tabs_screen.dart
+++ deli_meals/lib/screens/tabs_screen.dart
@@ -9,9 +9,15 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
-  List<Widget> _pages = [
-    CategoriesScreen(),
-    FavoritesScreen(),
+  List<Map<String, Object>> _pages = [
+    {
+      'page': CategoriesScreen(),
+      'title': 'Categories'
+    },
+    {
+      'page': FavoritesScreen(),
+      'title': 'Your Favorites'
+    }
   ];
 
   int _selectedPageIndex = 0;
@@ -26,9 +32,9 @@ class _TabsScreenState extends State<TabsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Meals'),
+        title: Text(_pages[_selectedPageIndex]['title']),
       ),
-      body: _pages[_selectedPageIndex],
+      body: _pages[_selectedPageIndex]['page'],
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
         backgroundColor: Theme.of(context).primaryColor,
