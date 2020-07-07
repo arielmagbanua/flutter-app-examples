37abf34de74012c364d9c40d269d63437d08abd4
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-07-07 18:55:24 +0800

    Theming

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -13,8 +13,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
+        fontFamily: 'Lato',
       ),
       home: ProductsOverviewScreen(),
     );
