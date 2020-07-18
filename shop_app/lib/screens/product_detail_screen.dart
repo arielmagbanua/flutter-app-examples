ed13417d7f2de8bb7e47434c6c47844007ab967f
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-07-18 17:39:40 +0800

    Code clean up.

--- shop_app/lib/screens/product_detail_screen.dart
+++ shop_app/lib/screens/product_detail_screen.dart
@@ -4,11 +4,6 @@ import 'package:provider/provider.dart';
 import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-//  final String title;
-//  final double price;
-//
-//  ProductDetailScreen(this.title, this.price);
-
   static const routeName = '/product-detail';
 
   @override
