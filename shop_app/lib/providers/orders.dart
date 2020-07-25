e569285d6b168fb51042d3cdd966f533dd7c0a7b
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-07-25 15:46:09 +0800

    Added fetching of orders.

--- shop_app/lib/providers/orders.dart
+++ shop_app/lib/providers/orders.dart
@@ -26,6 +26,38 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
+  Future<void> fetchAndSetOrders() async {
+    const url = "https://udemy-training-af9d1.firebaseio.com/orders.json";
+    final response = await http.get(url);
+    final List<OrderItem> loadedOrders = [];
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+
+    if (extractedData == null) {
+      return;
+    }
+
+    extractedData.forEach((orderId, orderData) {
+      loadedOrders.add(
+        OrderItem(
+          id: orderId,
+          amount: orderData['amount'],
+          products: (orderData['products'] as List<dynamic>)
+              .map((item) => CartItem(
+                    id: item['id'],
+                    title: item['title'],
+                    quantity: item['quantity'],
+                    price: item['price'],
+                  ))
+              .toList(),
+          dateTime: DateTime.parse(orderData['dateTime']),
+        ),
+      );
+    });
+
+    _orders = loadedOrders.reversed.toList();
+    notifyListeners();
+  }
+
   Future<void> addOrder(List<CartItem> cartProducts, double total) async {
     const url = "https://udemy-training-af9d1.firebaseio.com/orders.json";
     final timestamp = DateTime.now();
