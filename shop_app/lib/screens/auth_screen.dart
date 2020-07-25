24fd8fda592335b627173e7200944b70a8cfd739
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-07-25 23:24:54 +0800

    Used animation builder.

--- shop_app/lib/screens/auth_screen.dart
+++ shop_app/lib/screens/auth_screen.dart
@@ -129,7 +129,7 @@ class _AuthCardState extends State<AuthCard>
       ),
     );
 
-    _heightAnimation.addListener(() => setState(() {}));
+    // _heightAnimation.addListener(() => setState(() {}));
   }
 
   @override
@@ -231,14 +231,14 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
+      child: AnimatedBuilder(animation: _heightAnimation, builder: (ctx, ch) => Container(
         // height: _authMode == AuthMode.Signup ? 320 : 260,
-        height: _heightAnimation.value.height,
-        constraints:
-            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
-        width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
-        child: Form(
+          height: _heightAnimation.value.height,
+          constraints:
+          BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+          width: deviceSize.width * 0.75,
+          padding: EdgeInsets.all(16.0),
+          child: ch), child: Form(
           key: _formKey,
           child: SingleChildScrollView(
             child: Column(
