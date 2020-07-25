d033b3dbb7a6cec7f350e08017b6a7b7bdf404d1
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-07-26 03:03:02 +0800

    Added animation for product images.

--- shop_app/lib/widgets/product_item.dart
+++ shop_app/lib/widgets/product_item.dart
@@ -23,8 +23,9 @@ class ProductItem extends StatelessWidget {
               arguments: product.id,
             );
           },
-          child: Image.network(
-            product.imageUrl,
+          child: FadeInImage(
+            placeholder: AssetImage('assets/images/product-placeholder.png'),
+            image: NetworkImage(product.imageUrl),
             fit: BoxFit.cover,
           ),
         ),
