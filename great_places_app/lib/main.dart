9445cf3c083c3f67e515501fda3230ceed568b60
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-07-26 17:12:00 +0800

    Added AddPlaceScreen to the list of routes.

--- great_places_app/lib/main.dart
+++ great_places_app/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import './providers/great_places.dart';
 import './screens/places_list_screen.dart';
+import './screens/add_place_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -20,6 +21,9 @@ class MyApp extends StatelessWidget {
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
         home: PlacesListScreen(),
+        routes: {
+          AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
+        },
       ),
     );
   }
