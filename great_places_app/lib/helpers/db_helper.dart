8c555635aacb29a3049ac5227a7908a6deca8507
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-07-26 23:17:13 +0800

    Added new fields.

--- great_places_app/lib/helpers/db_helper.dart
+++ great_places_app/lib/helpers/db_helper.dart
@@ -8,7 +8,7 @@ class DBHelper {
     return sql.openDatabase(path.join(dbPath, 'places.db'),
         onCreate: (db, version) {
           return db.execute(
-              'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT)');
+              'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, loc_lat REAL, loc_lng REAL, address TEXT)');
         }, version: 1);
   }
 
