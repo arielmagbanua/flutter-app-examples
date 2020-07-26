82f2bb4f3ebc6690f9f9c9621fb76ca9f81c2159
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-07-26 18:37:00 +0800

    Added null check.

--- great_places_app/lib/widgets/image_input.dart
+++ great_places_app/lib/widgets/image_input.dart
@@ -23,6 +23,11 @@ class _ImageInputState extends State<ImageInput> {
       source: ImageSource.camera,
       maxWidth: 600,
     );
+
+    if (imageFile == null) {
+      return;
+    }
+
     setState(() {
       _storedImage = File(imageFile.path);
     });
