5c18da4e51080593fa517b9b8f96cdfff703738a
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-07-26 23:20:32 +0800

    Added extracting of address via google geo encode api.

--- great_places_app/lib/helpers/location_helper.dart
+++ great_places_app/lib/helpers/location_helper.dart
@@ -1,7 +1,17 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 const GOOGLE_API_KEY = 'AIzaSyAN4U8WGjgdJdz-6Lh66WCcRYf-crFirLs';
 
 class LocationHelper {
   static String generateLocationPreviewImage({double latitude, double longitude}) {
     return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:C%7C$latitude,$longitude&key=$GOOGLE_API_KEY';
+  }
+
+  static Future<String> getPlaceAddress(double lat, double lng) async {
+    final url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=$GOOGLE_API_KEY';
+    final response = await http.get(url);
+    return json.decode(response.body)['results'][0]['formatted_address'];
   }
 }
