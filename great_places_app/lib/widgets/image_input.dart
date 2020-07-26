15e0b29210f261804573b6298c72e0ac3b06b9eb
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-07-26 17:09:26 +0800

    Added ImageInput widget.

---
+++ great_places_app/lib/widgets/image_input.dart
@@ -0,0 +1,51 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class ImageInput extends StatefulWidget {
+  @override
+  _ImageInputState createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  File _storedImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(
+          width: 150,
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              color: Colors.grey,
+            ),
+          ),
+          child: _storedImage != null
+              ? Image.file(
+                  _storedImage,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                )
+              : Text(
+                  'No Image Taken',
+                  textAlign: TextAlign.center,
+                ),
+          alignment: Alignment.center,
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        Expanded(
+          child: FlatButton.icon(
+            icon: Icon(Icons.camera),
+            label: Text('Take Picture'),
+            textColor: Theme.of(context).primaryColor,
+          ),
+        ),
+      ],
+    );
+  }
+}
