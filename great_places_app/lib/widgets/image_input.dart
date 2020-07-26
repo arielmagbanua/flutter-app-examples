741e1d597611496c69d3281afac4b90374dcf472
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-07-26 17:33:13 +0800

    Added Image picker for taking a picture.

--- great_places_app/lib/widgets/image_input.dart
+++ great_places_app/lib/widgets/image_input.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   @override
@@ -9,6 +10,14 @@ class ImageInput extends StatefulWidget {
 
 class _ImageInputState extends State<ImageInput> {
   File _storedImage;
+
+  Future<void> _takePicture() async {
+    final picker = ImagePicker();
+    final imageFile = await picker.getImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,6 +52,7 @@ class _ImageInputState extends State<ImageInput> {
             icon: Icon(Icons.camera),
             label: Text('Take Picture'),
             textColor: Theme.of(context).primaryColor,
+            onPressed: _takePicture,
           ),
         ),
       ],
