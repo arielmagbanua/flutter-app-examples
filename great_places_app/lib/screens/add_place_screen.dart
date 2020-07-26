1bd4bc779ea6097a18835a1f933250c174936c18
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-07-26 19:38:09 +0800

    Added location input widget.

--- great_places_app/lib/screens/add_place_screen.dart
+++ great_places_app/lib/screens/add_place_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../widgets/image_input.dart';
+import '../widgets/location_input.dart';
 import '../providers/great_places.dart';
 
 class AddPlaceScreen extends StatefulWidget {
@@ -58,6 +59,10 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                       height: 10,
                     ),
                     ImageInput(_selectImage),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    LocationInput(),
                   ],
                 ),
               ),
