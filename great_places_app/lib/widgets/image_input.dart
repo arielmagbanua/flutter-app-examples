1d9ba11fbdcb761827080536b82fd65559c59953
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-07-26 18:11:36 +0800

    Added saving of file image to storage of the device.

--- great_places_app/lib/widgets/image_input.dart
+++ great_places_app/lib/widgets/image_input.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
 
 class ImageInput extends StatefulWidget {
   @override
@@ -17,6 +19,12 @@ class _ImageInputState extends State<ImageInput> {
       source: ImageSource.camera,
       maxWidth: 600,
     );
+    setState(() {
+      _storedImage = File(imageFile.path);
+    });
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName =  path.basename(_storedImage.path);
+    final saveImage = await _storedImage.copy('${appDir.path}/$fileName');
   }
 
   @override
