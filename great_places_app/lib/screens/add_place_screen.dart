ff8c3151010b1d93a8d42dff7b77e218e84b7e30
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-07-26 17:11:17 +0800

    Added needed widgets.

--- great_places_app/lib/screens/add_place_screen.dart
+++ great_places_app/lib/screens/add_place_screen.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/image_input.dart';
 
 class AddPlaceScreen extends StatefulWidget {
   static const routeName = '/add-place';
@@ -8,6 +11,8 @@ class AddPlaceScreen extends StatefulWidget {
 }
 
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,12 +20,34 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
         title: Text('Add a New Place'),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Text('User Inputs...'),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Title'),
+                      controller: _titleController,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    ImageInput()
+                  ],
+                ),
+              ),
+            ),
+          ),
           RaisedButton.icon(
             icon: Icon(Icons.add),
             label: Text('Add Place'),
             onPressed: () {},
+            elevation: 0,
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            color: Theme.of(context).accentColor,
           )
         ],
       ),
