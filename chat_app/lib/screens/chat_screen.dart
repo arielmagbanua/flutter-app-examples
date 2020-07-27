975b211a15d2ae8eb874f24a7f16b178c0e05a9a
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-07-27 22:59:19 +0800

    Added reading and creating of documents.

--- chat_app/lib/screens/chat_screen.dart
+++ chat_app/lib/screens/chat_screen.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text('This works'),
-        ),
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection('chats/LELay6YuJSGQPrCStR8J/messages')
+            .snapshots(),
+        builder: (ctx, streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          final documents = streamSnapshot.data.documents;
+          return ListView.builder(
+            itemCount: streamSnapshot.data.documents.length,
+            itemBuilder: (ctx, index) => Container(
+              padding: EdgeInsets.all(8),
+              child: Text(documents[index]['text']),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance
+              .collection('chats/LELay6YuJSGQPrCStR8J/messages')
+              .add(
+            {
+              'text': 'This was added by clicking the button!',
+            },
+          );
+        },
       ),
     );
   }
