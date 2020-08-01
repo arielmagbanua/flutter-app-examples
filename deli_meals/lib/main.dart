23aefd0b6b2c73c1f2c8ad273f20a2b68147c78a
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-01 23:54:19 +0800

    Replaced deprecated codes with new codes.

--- deli_meals/lib/main.dart
+++ deli_meals/lib/main.dart
@@ -83,13 +83,13 @@ class _MyAppState extends State<MyApp> {
         canvasColor: Color.fromRGBO(255, 254, 229, 1),
         fontFamily: 'Raleway',
         textTheme: ThemeData.light().textTheme.copyWith(
-            body1: TextStyle(
+            bodyText1: TextStyle(
               color: Color.fromRGBO(20, 51, 51, 1),
             ),
-            body2: TextStyle(
+            bodyText2: TextStyle(
               color: Color.fromRGBO(20, 51, 51, 1),
             ),
-            title: TextStyle(
+            headline6: TextStyle(
               fontSize: 20,
               fontFamily: 'RobotoCondensed',
               fontWeight: FontWeight.bold,
