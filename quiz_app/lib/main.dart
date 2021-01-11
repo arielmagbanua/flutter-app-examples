9839df0372bdbc7f4e78c7c305e0e5949fb65671
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-01-11 14:29:01 +0800

    Custom widgets.

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './question.dart';
 
 void main() => runApp(MyApp());
 
@@ -31,7 +32,7 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Text(questions[_questionIndex]),
+            Question(questions[_questionIndex]),
             RaisedButton(
               child: Text('Answer 1'),
               onPressed: _answerQuestion,
