50940d8580e31808652d218c211672f43c983ad0
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-02-04 22:17:26 +0800

    renamed some files.

--- flutter_bloc_cubit_app/lib/cubit_main.dart
+++ flutter_bloc_cubit_app/lib/cubit_main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_bloc_cubit_app/CounterCubit.dart';
+import 'package:flutter_bloc_cubit_app/counter_cubit.dart';
 
 void main() {
   runApp(MyApp());
--- flutter_bloc_cubit_app/lib/main.dart
+++ flutter_bloc_cubit_app/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import './CounterBloc.dart';
+import './counter_bloc.dart';
 
 void main() {
   runApp(MyApp());
