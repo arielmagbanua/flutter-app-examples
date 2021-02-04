aa511e3882527f29a992f511cd60bd83befa5af0
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-02-04 21:36:43 +0800

    Added sample class implementation of bloc and cubit.

---
+++ flutter_bloc_cubit_app/lib/CounterBloc.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+enum CounterEvent { increment, decrement }
+
+class CounterBloc extends Bloc<CounterEvent, int> {
+  CounterBloc() : super(0);
+
+  @override
+  Stream<int> mapEventToState(CounterEvent event) async* {
+    switch (event) {
+      case CounterEvent.increment:
+        yield state + 1;
+        break;
+
+      case CounterEvent.decrement:
+        yield state - 1;
+        break;
+    }
+  }
+}
