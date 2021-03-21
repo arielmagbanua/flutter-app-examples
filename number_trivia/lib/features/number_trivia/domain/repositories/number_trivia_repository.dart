794760f8bd93624cf180d64e4385c66185d8a72d
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-03-21 13:23:59 +0800

    Added number trivia repo.

---
+++ number_trivia/lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+
+import 'package:features.number_trivia/core/error/failures.dart';
+import 'package:features.number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+
+abstract class NumberTriviaRepository {
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number);
+
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia();
+}
