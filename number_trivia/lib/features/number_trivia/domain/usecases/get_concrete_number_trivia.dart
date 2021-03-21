28d5349a6cfc4483c03caacd24e80bdef3bfc726
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-03-21 23:31:33 +0800

    Moved params.

--- number_trivia/lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ number_trivia/lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,7 +1,6 @@
 import 'package:dartz/dartz.dart';
-import 'package:equatable/equatable.dart';
+
 import 'package:features.number_trivia/core/usecases/usecase.dart';
-import 'package:meta/meta.dart';
 import 'package:features.number_trivia/core/error/failures.dart';
 import 'package:features.number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:features.number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
@@ -15,13 +14,4 @@ class GetConcreteNumberTrivia implements UseCase<NumberTrivia, Params> {
   Future<Either<Failure, NumberTrivia>> call(Params params) async {
     return await repository.getConcreteNumberTrivia(params.number);
   }
-}
-
-class Params extends Equatable {
-  final int number;
-
-  Params({@required this.number});
-
-  @override
-  List<Object> get props => [number];
 }
