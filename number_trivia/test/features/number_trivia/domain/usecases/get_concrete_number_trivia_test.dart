a73c6e30771756a4b155eb49f9547231224242c9
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-03-21 22:52:54 +0800

    Renamed the file.

--- number_trivia/test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
+++ number_trivia/test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:features.number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:features.number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
-import 'package:features.number_trivia/features/number_trivia/domain/usecases/GetConcreteNumberTrivia.dart';
+import 'package:features.number_trivia/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
 import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
 
