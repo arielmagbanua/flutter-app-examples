0a1400af88380989872ee0c2b3dc03e8e8e56d7d
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-03-23 20:03:21 +0800

    Added number_trivia_local_data_source.dart

--- number_trivia/lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
+++ number_trivia/lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
@@ -1,3 +1,9 @@
+import 'dart:convert';
+
+import 'package:features.number_trivia/core/error/exceptions.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:meta/meta.dart';
+
 import 'package:features.number_trivia/features/number_trivia/data/models/number_trivia_model.dart';
 
 abstract class NumberTriviaLocalDataSource {
@@ -8,4 +14,31 @@ abstract class NumberTriviaLocalDataSource {
   Future<NumberTriviaModel> getLastNumberTrivia();
 
   Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache);
+}
+
+class NumberTriviaLocalDataSourceImpl implements NumberTriviaLocalDataSource {
+  static const CACHED_NUMBER_TRIVIA = 'CACHED_NUMBER_TRIVIA';
+
+  final SharedPreferences sharedPreferences;
+
+  NumberTriviaLocalDataSourceImpl({
+    @required this.sharedPreferences,
+  });
+
+  @override
+  Future<NumberTriviaModel> getLastNumberTrivia() {
+    final jsonString = sharedPreferences.getString(CACHED_NUMBER_TRIVIA);
+
+    if (jsonString != null) {
+      return Future.value(NumberTriviaModel.fromJson(json.decode(jsonString)));
+    }
+
+    throw CacheException();
+  }
+
+  @override
+  Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache) {
+    // TODO: implement cacheNumberTrivia
+    throw UnimplementedError();
+  }
 }
