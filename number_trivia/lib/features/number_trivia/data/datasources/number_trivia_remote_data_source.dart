c10e2b6cb655b65303f131980e7deb0241222ba0
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-03-23 21:45:11 +0800

    Finished the implementation of remote data source and added tests.

--- number_trivia/lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
+++ number_trivia/lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
@@ -1,3 +1,10 @@
+import 'dart:convert';
+
+import 'package:features.number_trivia/core/error/exceptions.dart';
+import 'package:features.number_trivia/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:http/http.dart' as http;
+import 'package:meta/meta.dart';
+
 import 'package:features.number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 
 abstract class NumberTriviaRemoteDataSource {
@@ -10,4 +17,33 @@ abstract class NumberTriviaRemoteDataSource {
   ///
   /// Throws a [ServerException] for all error codes.
   Future<NumberTrivia> getRandomNumberTrivia();
+}
+
+class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource {
+  final http.Client client;
+
+  NumberTriviaRemoteDataSourceImpl({
+    @required this.client,
+  });
+
+  @override
+  Future<NumberTrivia> getConcreteNumberTrivia(int number) =>
+      _getTriviaFromUrl('http://numbersapi.com/$number');
+
+  @override
+  Future<NumberTrivia> getRandomNumberTrivia() =>
+      _getTriviaFromUrl('http://numbersapi.com/random');
+
+  Future<NumberTriviaModel> _getTriviaFromUrl(String url) async {
+    final response = await client.get(
+      Uri.parse(url),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode == 200) {
+      return NumberTriviaModel.fromJson(json.decode(response.body));
+    }
+
+    throw ServerException();
+  }
 }
