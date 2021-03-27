3e20ea6a4eedc6ac0d237f40f459b3720e42038b
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-03-27 11:59:31 +0800

    Added number trivia bloc.

--- number_trivia/lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ number_trivia/lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -3,18 +3,39 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
+import 'package:features.number_trivia/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 import 'package:features.number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:features.number_trivia/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:features.number_trivia/core/util/input_converter.dart';
 
 part './number_trivia_event.dart';
+
 part './number_trivia_state.dart';
 
+const String SERVER_FAILURE_MESSAGE = 'Server failure';
+const String CACHE_FAILURE_MESSAGE = 'Cache failure';
+const String INVALID_INPUT_FAILURE_MESSAGE =
+    'Invalid Input - The number must be a positive integer or zero';
+
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
-  NumberTriviaBloc() : super(NumberTriviaInitial());
+  final GetConcreteNumberTrivia getConcreteNumberTrivia;
+  final GetRandomNumberTrivia getRandomNumberTrivia;
+  final InputConverter inputConverter;
+
+  NumberTriviaBloc({
+    @required GetConcreteNumberTrivia concrete,
+    @required GetRandomNumberTrivia random,
+    @required this.inputConverter,
+  })  : this.getConcreteNumberTrivia = concrete,
+        this.getRandomNumberTrivia = random,
+        super(Empty());
 
   @override
   Stream<NumberTriviaState> mapEventToState(
     NumberTriviaEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    if (event is GetTriviaForConcreteNumber) {
+      inputConverter.stringToUnsignedInteger(event.numberString);
+    }
   }
 }
