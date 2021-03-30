8bec8f1b319f4f7b40bd88a9ff2d3268e68e634f
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-03-30 21:15:59 +0800

    removed the old copy of number trivia.

--- number_trivia/android/app/src/main/kotlin/com/malupets/number_trivia/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.malupets.number_trivia
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- number_trivia/lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dartz/dartz.dart';
-
-import 'package:features.number_trivia/core/error/failures.dart';
-import 'package:features.number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
-
-abstract class NumberTriviaRepository {
-  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number);
-  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia();
-}
