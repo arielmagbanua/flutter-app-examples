db611b5f6d72ade4aac35a89d8760d1fd9e4ec17
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-04-28 22:03:57 +0800

    Added waves background and used it.

--- flutter_timer/lib/main.dart
+++ flutter_timer/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_timer/waves_background.dart';
 
 import 'timer_state.dart';
 import 'ticker.dart';
@@ -39,33 +40,38 @@ class Timer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Flutter Timer')),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      body: Stack(
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 100.0),
-            child: Center(
-              child: BlocBuilder<TimerBloc, TimerState>(
-                builder: (context, state) {
-                  final String minutesStr = ((state.duration / 60) % 60)
-                      .floor()
-                      .toString()
-                      .padLeft(2, '0');
-                  final String secondsStr =
+          WavesBackground(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 100.0),
+                child: Center(
+                  child: BlocBuilder<TimerBloc, TimerState>(
+                    builder: (context, state) {
+                      final String minutesStr = ((state.duration / 60) % 60)
+                          .floor()
+                          .toString()
+                          .padLeft(2, '0');
+                      final String secondsStr =
                       (state.duration % 60).floor().toString().padLeft(2, '0');
-                  return Text(
-                    '$minutesStr:$secondsStr',
-                    style: Timer.timerTextStyle,
-                  );
-                },
+                      return Text(
+                        '$minutesStr:$secondsStr',
+                        style: Timer.timerTextStyle,
+                      );
+                    },
+                  ),
+                ),
               ),
-            ),
-          ),
-          BlocBuilder<TimerBloc, TimerState>(
-            buildWhen: (previousState, state) =>
+              BlocBuilder<TimerBloc, TimerState>(
+                buildWhen: (previousState, state) =>
                 state.runtimeType != previousState.runtimeType,
-            builder: (context, state) => a.Actions(),
+                builder: (context, state) => a.Actions(),
+              ),
+            ],
           ),
         ],
       ),
