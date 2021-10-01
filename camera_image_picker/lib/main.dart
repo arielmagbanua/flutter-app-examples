caed5516bb714161d4c983b2a99956e8b4751430
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-10-01 22:40:41 +0800

    renamed.

--- camera_image_picker/lib/main.dart
+++ camera_image_picker/lib/main.dart
@@ -14,11 +14,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Camera Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Camera Demo Page'),
     );
   }
 }
