0ad43b5734b6edde462e1c2e3931a73cf50467ef
unknown <magbanua.c.ariel@gmail.com>
2022-05-05 18:48:50 +0800

    Added starter files.

---
+++ flutter_twitter/lib/news_service.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_lorem/flutter_lorem.dart';
+
+import 'article.dart';
+
+/// A News service simulating communication with a server.
+class NewsService {
+  // Simulating a remote database
+  final _articles = List.generate(
+    10,
+        (_) => Article(
+      title: lorem(paragraphs: 1, words: 3),
+      content: lorem(paragraphs: 10, words: 500),
+    ),
+  );
+
+  Future<List<Article>> getArticles() async {
+    await Future.delayed(const Duration(seconds: 1));
+    return _articles;
+  }
+}
