039b6820ca59b449157bf22705e01fb5007913e7
unknown <magbanua.c.ariel@gmail.com>
2022-06-13 18:51:28 +0800

    Updated packages and gradle and fixed deprecated codes.

--- deli_meals/lib/widgets/category_item.dart
+++ deli_meals/lib/widgets/category_item.dart
@@ -29,7 +29,7 @@ class CategoryItem extends StatelessWidget {
         padding: const EdgeInsets.all(15),
         child: Text(
           title,
-          style: Theme.of(context).textTheme.title,
+          style: Theme.of(context).textTheme.headline6,
         ),
         decoration: BoxDecoration(
           gradient: LinearGradient(
