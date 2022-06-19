89a7c07d50897948a43096903554dd56140b7684
unknown <magbanua.c.ariel@gmail.com>
2022-06-19 20:27:09 +0800

    Code cleanup.

--- chat_app/functions/index.js
+++ chat_app/functions/index.js
@@ -3,14 +3,6 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 exports.myFunction = functions.firestore
   .document('chat/{message}')
   .onCreate((snapshot, context) => {
