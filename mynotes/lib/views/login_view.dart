bc352a42b8de43c81b367c94cafcb0b0fee157dd
unknown <magbanua.c.ariel@gmail.com>
2022-07-10 15:41:35 +0800

    Added error alert dialog.

--- mynotes/lib/views/login_view.dart
+++ mynotes/lib/views/login_view.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtools show log;
 
 import '../constants/routes.dart';
+import '../utilities/show_error_dialog.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -56,7 +56,6 @@ class _LoginViewState extends State<LoginView> {
             controller: _password,
           ),
           TextButton(
-            child: const Text('Login'),
             onPressed: () async {
               final email = _email.text;
               final password = _password.text;
@@ -73,16 +72,29 @@ class _LoginViewState extends State<LoginView> {
                 );
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'user-not-found') {
-                  devtools.log('User not found');
+                  await showErrorDialog(
+                    context,
+                    'User not found',
+                  );
                 } else if (e.code == 'wrong-password') {
-                  devtools.log('SOMETHING ELSE HAPPENED');
-                  devtools.log(e.code);
+                  await showErrorDialog(
+                    context,
+                    'Wrong credentials',
+                  );
+                } else {
+                  await showErrorDialog(
+                    context,
+                    'Error: ${e.code}',
+                  );
                 }
               } catch (e) {
-                devtools.log('something bad happened');
-                devtools.log(e.toString());
+                await showErrorDialog(
+                  context,
+                  e.toString(),
+                );
               }
             },
+            child: const Text('Login'),
           ),
           TextButton(
             onPressed: () {
