de6c41aa9ada7808cb73a8f77bcaa8fd1296ebc7
unknown <magbanua.c.ariel@gmail.com>
2022-08-27 21:57:49 +0800

    Improved the creation of stream controller.

--- mynotes/lib/services/crud/notes_service.dart
+++ mynotes/lib/services/crud/notes_service.dart
@@ -14,11 +14,19 @@ class NotesService {
 
   // singleton implementation
   static final NotesService _shared = NotesService._sharedInstance();
-  NotesService._sharedInstance();
+
+  NotesService._sharedInstance() {
+    _notesStreamController = StreamController<List<DatabaseNote>>.broadcast(
+      onListen: () {
+        // when new listener listens populate the notes
+        _notesStreamController.sink.add(_notes);
+      },
+    );
+  }
+
   factory NotesService() => _shared;
 
-  final _notesStreamController =
-      StreamController<List<DatabaseNote>>.broadcast();
+  late final StreamController<List<DatabaseNote>> _notesStreamController;
 
   Stream<List<DatabaseNote>> get allNotes => _notesStreamController.stream;
 
