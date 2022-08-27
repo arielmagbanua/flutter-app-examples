6247f4a021f965453b3cb3dab1305cdecb4d8269
unknown <magbanua.c.ariel@gmail.com>
2022-08-27 20:30:18 +0800

    Implemented singleton.

--- mynotes/lib/services/crud/notes_service.dart
+++ mynotes/lib/services/crud/notes_service.dart
@@ -12,8 +12,15 @@ class NotesService {
 
   List<DatabaseNote> _notes = [];
 
+  // singleton implementation
+  static final NotesService _shared = NotesService._sharedInstance();
+  NotesService._sharedInstance();
+  factory NotesService() => _shared;
+
   final _notesStreamController =
       StreamController<List<DatabaseNote>>.broadcast();
+
+  Stream<List<DatabaseNote>> get allNotes => _notesStreamController.stream;
 
   Future<DatabaseUser> getOrCreateUser({required String email}) async {
     try {
