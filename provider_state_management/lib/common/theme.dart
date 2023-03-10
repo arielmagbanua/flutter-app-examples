e97db9a0cdc6fe1f90052c45dd4710c52c3a9707
unknown <magbanua.c.ariel@gmail.com>
2023-03-11 00:03:02 +0800

    Added provider change notifier example from flutter website.

---
+++ provider_state_management/lib/common/theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+final appTheme = ThemeData(
+  colorSchemeSeed: Colors.yellow,
+  useMaterial3: true,
+  textTheme: const TextTheme(
+    displayLarge: TextStyle(
+      fontFamily: 'Corben',
+      fontWeight: FontWeight.w700,
+      fontSize: 24,
+      color: Colors.black,
+    ),
+  ),
+);
