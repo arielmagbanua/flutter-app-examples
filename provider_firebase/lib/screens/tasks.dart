42d0ea6242f82ed088f9a41470d9a83133b7b981
unknown <magbanua.c.ariel@gmail.com>
2023-03-12 21:06:16 +0800

    Added pages and routes.

---
+++ provider_firebase/lib/screens/tasks.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Tasks extends StatelessWidget {
+  const Tasks({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
